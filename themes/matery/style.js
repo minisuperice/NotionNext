c4a8fc1f496e378675f06c32d996bb73895774ec
minisuperice <55423095+minisuperice@users.noreply.github.com>
2024-06-25 17:01:54 +0800

    Update style.js

--- themes/matery/style.js
+++ themes/matery/style.js
@@ -44,12 +44,12 @@ const Style = () => {
       }
 
       ::-webkit-scrollbar-thumb {
-        background-color: #4338ca;
+        background-color: #ccd0d9;
       }
 
       * {
         scrollbar-width: thin;
-        scrollbar-color: #4338ca transparent;
+        scrollbar-color: #ccd0d9 transparent;
       }
     `}</style>
   )
